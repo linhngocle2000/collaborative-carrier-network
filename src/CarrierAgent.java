f32a10ecb98714db6095df6e20fc8af03c05f9ca
lle <lle@novomind.com>
2022-05-02 22:20:13 +0200

    first agent class

---
+++ src/CarrierAgent.java
@@ -0,0 +1,9 @@
+import jade.core.Agent;
+
+public class CarrierAgent extends Agent {
+
+    @Override
+    protected void setup() {
+        System.out.println("Hello! Carrier agent "+getAID().getName()+" is ready.");
+    }
+}
