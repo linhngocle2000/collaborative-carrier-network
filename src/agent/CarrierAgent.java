3d20474940bdf29a6a98b2f829de1e5eb4b9b533
Kien Giang Phan <kien.phan@tuhh.de>
2022-05-04 19:40:40 +0000

    Update CarrierAgent.java - register agent into agent list

--- src/agent/CarrierAgent.java
+++ src/agent/CarrierAgent.java
@@ -1,11 +1,38 @@
 package agent;
 
+import java.util.ArrayList;
+
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
 
 public class CarrierAgent extends Agent {
+    private ArrayList<Agent> registeredAgents = new ArrayList<Agent>();
 
-    @Override
-    protected void setup() {
-        System.out.println("Hello! Carrier agent "+getAID().getName()+" is ready.");
-    }
+
+   // Register a new agent
+   public void register(Agent agent, ServiceDescription sd) {
+      DFAgentDescription dfd = new DFAgentDescription();
+      dfd.setName(getAID());
+      dfd.addServices(sd);
+
+      try {
+         registeredAgents.add(agent);
+         DFService.register(agent, dfd);
+         System.out.println(agent.getName() + " registerd as: " + sd.getType());
+      } catch (FIPAException e) {
+         e.printStackTrace();
+      }
+   }
+
+
+   // Registers the agent with the Directory Facilitator as a Carrier
+   protected void setup() {
+      ServiceDescription sd = new ServiceDescription();
+      sd.setType("Carrier");
+      sd.setName(getLocalName());
+      register(this, sd);
+   }
 }
