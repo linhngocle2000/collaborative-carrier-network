45fe23316d0a900bc83ff527a9ebb3d1aa44130d
lle <lle@novomind.com>
2022-05-06 01:01:05 +0200

    Implement AuctioneerAgent which confirm requests from CarrierAgent. Implement CarrierAgent which send join/cancel request to AuctioneerAgent.

--- src/agent/AuctioneerAgent.java
+++ src/agent/AuctioneerAgent.java
@@ -1,19 +1,76 @@
 package agent;
 
 import jade.core.Agent;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.core.AID;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+
+import java.util.ArrayList;
+
 
 public class AuctioneerAgent extends Agent {
-   private Helper helper;
 
-   // Registers the agent with the Directory Facilitator as an Auctioneer
+   // New load to auction
+   private String load;
+   // List of bidders
+   private ArrayList<AID> bidders = new ArrayList<>();
+
+   // Agent initialization
    protected void setup() {
-      helper = Helper.getInstance();
-      ServiceDescription sd = new ServiceDescription();
-      sd.setType("Auctioneer");
-      sd.setName(getLocalName());
-      helper.register(this, sd);
+      // Printout a welcome message
+      System.out.println("Hello! Auctioneer-agent "+getAID().getName()+" is ready.");
+      // Get the name of new load that agent will auction
+      Object[] args = getArguments();
+      if (args != null && args.length > 0) {
+         load = (String) args[0];
+         System.out.println("Trying to auction "+load);
+      }
+      else {
+         // Make the agent terminate immediately
+         System.out.println("No load specified");
+         doDelete();
+      }
+      // Add the behaviour confirming requests for requests from carrier agents
+      addBehaviour(new ConfirmRequestsServer());
    }
-	
-	
+
+   /**
+    Inner class ConfirmRequestsServer.
+    If Auctioneer-agent receives a "Join" message, it adds the sender to the bidders queue.
+    If Auctioneer-agent receives a "Cancel" message, it removes the sender from the bidders queue.
+    */
+   private class ConfirmRequestsServer extends CyclicBehaviour {
+      public void action() {
+         ACLMessage msg = myAgent.receive();
+         if (msg != null) {
+            ACLMessage reply = msg.createReply();
+            switch(msg.getContent()){
+               // "Join" message received
+               case "Join":
+                  // Sender is already in bidders list, then no need to add
+                  if (bidders.contains(msg.getSender())) {
+                     reply.setPerformative(ACLMessage.INFORM);
+                     reply.setContent("Auction already joined");
+                  } else {
+                     // else adds to bidders list
+                     bidders.add(msg.getSender());
+                     reply.setPerformative(ACLMessage.CONFIRM);
+                     reply.setContent("Auction joined");
+                  }
+               // "Cancel" msg received
+               case "Cancel":
+                  // Remove sender from list of bidders
+                  bidders.remove(msg.getSender());
+                  reply.setPerformative(ACLMessage.CONFIRM);
+                  reply.setContent("Auction exit");
+            }
+            myAgent.send(reply);
+         }
+      }
+   }
+   // Put agent clean-up operations here
+   protected void takeDown() {
+      // Printout a dismissal message
+      System.out.println("Auctioneer agent "+getAID().getName()+" terminating.");
+   }
 }
--- src/agent/CarrierAgent.java
+++ src/agent/CarrierAgent.java
@@ -1,17 +1,59 @@
 package agent;
 
+import jade.core.AID;
 import jade.core.Agent;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+
 
 public class CarrierAgent extends Agent {
-   private Helper helper;
 
-   // Registers the agent with the Directory Facilitator as a Carrier
+   private AID auctioneerAgents ;
+
+   // Agent initialization
    protected void setup() {
-      helper = Helper.getInstance();
-      ServiceDescription sd = new ServiceDescription();
-      sd.setType("Carrier");
-      sd.setName(getLocalName());
-      helper.register(this, sd);
+      // Printout a welcome message
+      System.out.println("Hello! Carrier-agent "+getAID().getName()+" is ready.");
+      // Get the name of auctioneer agent which hosts the auction
+      Object[] args = getArguments();
+      if (args != null && args.length > 0) {
+         auctioneerAgents = new AID((String) args[0], AID.ISLOCALNAME);
+         System.out.println("Trying to join auction of "+auctioneerAgents);
+      }
+      else {
+         // Make the agent terminate immediately
+         System.out.println("No load specified");
+         doDelete();
+      }
+   }
+
+   // Send "Join" message
+   private class SendRequest extends OneShotBehaviour {
+      public void action() {
+         ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+         msg.setSender(this.myAgent.getAID());
+         msg.setProtocol("auction-request");
+         msg.setContent("Join");
+         msg.addReceiver(auctioneerAgents);
+         send(msg);
+      }
+   }
+
+   // Send "Cancel" message
+   private class CancelRequest extends OneShotBehaviour {
+      public void action() {
+         ACLMessage msg = new ACLMessage(ACLMessage.CANCEL);
+         msg.setSender(this.myAgent.getAID());
+         msg.setProtocol("auction-request");
+         msg.setContent("Cancel");
+         msg.addReceiver(auctioneerAgents);
+         send(msg);
+      }
+   }
+
+   // Put agent clean-up operations here
+   protected void takeDown() {
+      // Printout a dismissal message
+      System.out.println("Carrier-agent "+getAID().getName()+" terminating.");
    }
 }
