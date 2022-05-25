8b85ed4c2c715b3c57b5c930708764cd5d8e6af6
Christian Kruse <c.kruse@tuhh.de>
2022-05-25 21:31:47 +0200

    Add getAgents command

--- backend/Route/agentRoute.php
+++ backend/Route/agentRoute.php
@@ -38,3 +38,30 @@ $handler->register('getAuctions', function ($data)
 	}
 	return $auctions;
 });
+
+$handler->register('getAgents', function ($data)
+{
+	TokenHelper::assertToken();
+
+	$db = Database::getConnection();
+	$result = $db->query("SELECT a.Username, a.Name AS UserDisplayname, ac.ID AS AuctionID, ac.Name AS AuctionName FROM `Agent` a JOIN `Auction` ac ON a.Username = ac.Auctioneer");
+	$agents = [];
+	while ($row = $result->fetch_assoc())
+	{
+		if (empty($agents[$row['Username']]))
+		{
+			$agents[$row['Username']] = [
+				'Username' => $row['Username'],
+				'Name' => $row['UserDisplayname'],
+				'Auctions' => [],
+			];
+		}
+
+		$agents[$row['Username']]['Auctions'][] = [
+			'ID' => $row['AuctionID'],
+			'Name' => $row['AuctionName'],
+			'Auctioneer' => $row['Username'],
+		];
+	}
+	return array_values($agents);
+});
