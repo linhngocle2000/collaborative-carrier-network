76386a6f433327b47314ebe33f2ba2853c449d54
Christian Kruse <c.kruse@tuhh.de>
2022-06-07 08:11:17 +0200

    Do not show errors for production

--- backend/index.php
+++ backend/index.php
@@ -5,11 +5,6 @@
 use CCN\Route\Handler;
 use CCN\Util\TokenHelper;
 
-// I want all the errors
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // phpinfo();
 
 header('Content-type: application/json');
@@ -17,19 +12,48 @@ header('Content-type: application/json');
 require __DIR__ . '/Util/autoload.php';
 require __DIR__ . '/Util/functions.php';
 
+function error($data)
+{
+	http_response_code(400);
+	echo json_encode([
+		'success' => false,
+		'error' => $data,
+	]);
+}
+
 try
 {
 	// Validate request format
 	// $method = $_SERVER['REQUEST_METHOD'];
 	$body = file_get_contents('php://input');
 	$body = json_decode($body, true);
+
 	if ($body === false)
 	{
-		throw new Exception(json_last_error_msg(), json_last_error());
+		error([
+			'message' => json_last_error_msg(),
+			'code' => json_last_error(),
+		]);
 	}
+
 	if (!array_key_exists('Cmd', $body))
 	{
-		throw new Exception("No command defined!");
+		error([
+			'message' => 'No command defined!',
+		]);
+	}
+
+	if (array_key_exists('Debug', $body) && $body['Debug'])
+	{
+		// I want all the errors
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
+	}
+	else
+	{
+		// Do not report errors for production
+		error_reporting(0);
 	}
 
 	// Set session token
@@ -53,13 +77,9 @@ try
 }
 catch (\Exception $ex)
 {
-	http_response_code(400);
-	echo json_encode([
-		"success" => false,
-		"error" => [
-			"message" => $ex->getMessage(),
-			"code" => $ex->getCode(),
-			"stacktrace" => $ex->getTraceAsString(),
-		]
+	error([
+		"message" => $ex->getMessage(),
+		"code" => $ex->getCode(),
+		"stacktrace" => $ex->getTraceAsString(),
 	]);
 }
