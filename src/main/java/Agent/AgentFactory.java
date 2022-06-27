226e1900c7e63f85d7714d4231d2419b99f1681b
Christian Kruse <c.kruse@tuhh.de>
2022-06-27 23:14:59 +0200

    Add min and max profit

--- src/main/java/Agent/AgentFactory.java
+++ src/main/java/Agent/AgentFactory.java
@@ -16,13 +16,15 @@ public class AgentFactory {
 	public static CarrierAgent carrierFromJSON(JSONObject json) {
 		String username = json.getString("Username");
 		String displayname = json.getString("Name");
+		double minProfit = json.getDouble("MinProfit");
+		double maxProfit = json.getDouble("MaxProfit");
 		float depotX = json.getFloat("DepotLat");
 		float depotY = json.getFloat("DepotLon");
 		float pickupBaserate = json.getFloat("PickupBaserate");
 		float externalTravelCost = json.getFloat("TravelCostPerKM");
 		float loadBaserate = json.getFloat("LoadBaserate");
 		float internalTravelCost = json.getFloat("InternalTravelCostPerKM");
-		return new CarrierAgent(username, displayname, depotX, depotY, pickupBaserate, externalTravelCost, loadBaserate, internalTravelCost);
+		return new CarrierAgent(username, displayname, minProfit, maxProfit, depotX, depotY, pickupBaserate, externalTravelCost, loadBaserate, internalTravelCost);
 	}
 
 	public static AuctioneerAgent auctioneerFromJSON(JSONObject json) {
--- src/main/java/Agent/CarrierAgent.java
+++ src/main/java/Agent/CarrierAgent.java
@@ -2,9 +2,11 @@ package Agent;
 
 public class CarrierAgent extends Agent {
 
-	public CarrierAgent(String username, String displayname, float depotX, float depotY, float pickupBaserate,
+	public CarrierAgent(String username, String displayname, double minProfit, double maxProfit, float depotX, float depotY, float pickupBaserate,
 			float externalTravelCost, float loadBaserate, float internalTravelCost) {
 		super(username, displayname);
+		this.minProfit = minProfit;
+		this.maxProfit = maxProfit;
 		this.depotX = depotX;
 		this.depotY = depotY;
 		this.pickupBaserate = pickupBaserate;
@@ -21,8 +23,19 @@ public class CarrierAgent extends Agent {
 	// Variables
 
 	private float depotX, depotY, pickupBaserate, externalTravelCost, loadBaserate, internalTravelCost;
+	private double minProfit, maxProfit;
 
 	// Getters
+
+	/** @return Minimum profit that a transport request must result in for the carrier to bid on it */
+	public double getMinProfit() {
+		return minProfit;
+	}
+
+	/** @return Profit threshold for own transport requests. Transport requests resulting in less profit will be auctioned off. */
+	public double getMaxProfit() {
+		return maxProfit;
+	}
 
 	public float getDepotX() {
 		return depotX;
